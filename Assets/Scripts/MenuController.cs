cd0f438ce1e64688c60a1e129114d37e0fc8911d
Guilherme Galarça Dutra <udutra@gmail.com>
2018-09-25 11:07:43 -0300

    Menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject panelAjuda;
+    public GameObject panelRanking;
+
+	public void Ajuda(bool open)
+    {
+        if (open)
+        {
+            panelAjuda.SetActive(true);
+        }
+        else
+        {
+            panelAjuda.SetActive(false);
+        }
+    }
+
+    public void Ranking(bool open)
+    {
+        if (open)
+        {
+            panelRanking.SetActive(true);
+        }
+        else
+        {
+            panelRanking.SetActive(false);
+        }
+    }
+}
